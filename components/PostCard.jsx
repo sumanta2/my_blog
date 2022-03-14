bf8129368e7cce2744a35c4c51e2a5f54e8fc78d
sumanta2 <sumantagorai.21@gmail.com>
2022-03-14 16:32:56 +0530

    2 code Setup

---
+++ components/PostCard.jsx
@@ -0,0 +1,13 @@
+
+const PostCard=({post})=> {
+    console.log("hello bro")
+  return (
+      
+    <div>
+        {post.title}
+        {post.excerpt}
+    </div>
+  )
+}
+
+export default PostCard
